095433a021a460e38ebebd0cfc3a3b5c4934229b
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-08-17 20:47:59 +0300

    fix issues

--- react/aws/src/Components/buckets/buckets.js
+++ react/aws/src/Components/buckets/buckets.js
@@ -1,161 +1,155 @@
-import { Component } from "react";
-import { NavLink, Outlet} from "react-router-dom";
 
+import React, { Component, useState, useEffect } from "react";
+import { NavLink, Outlet,useNavigate } from "react-router-dom";
 
-class Buckets extends Component{
+function Buckets() {
+    const [credential, setCredential] = useState({
+        accessId: "",
+        secretAccessId: "",
+    });
+    const [allBuckets, setAllBuckets] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState({ status: false, msg: "" });
 
-    constructor(){
-        super()
-        this.state={
-            credential:{
-                accessId:"",
-                secretAccessId:"",
-                
-            },
-            allBuckets:[],
-            loading:false,
-            errors:{
-                status:false,
-                msg:""
-            }
+    const getAccessId = (e) => {
+        setCredential((prevCredential) => ({
+            ...prevCredential,
+            accessId: e.target.value,
+        }));
+    };
+
+    const getSecretAccessId = (e) => {
+        setCredential((prevCredential) => ({
+            ...prevCredential,
+            secretAccessId: e.target.value,
+        }));
+    };
+
+    const submit = () => {
+        if (credential.accessId.length > 0 && credential.secretAccessId.length > 0) {
+            setErrors({ status: false, msg: "" })
+            setAllBuckets([])
+            navigateHome()
+            getData();
+        }else{
+            setErrors({ status: true, msg: "please provide credentials first." })
         }
-    }
-    getAccessId=(e)=>{
-        this.setState(prevState => {
-            let credential = Object.assign({}, prevState.credential);  
-            credential.accessId = e.target.value;  
-            return  {credential} ;                               
-        })
-    }
-    getSecretAccessId=(e)=>{
-        this.setState(prevState => {
-            let credential = Object.assign({}, prevState.credential);  
-            credential.secretAccessId = e.target.value;  
-            return  {credential} ;                               
-        })
+    };
 
-    }
+    const navigate = useNavigate();
 
-    submit=(e)=>{
-        if(this.getAccessId.length>0 && this.getSecretAccessId.length>0){
-            this.getData()
-        }
-        
-    }
-    getData(){
-        fetch("http://localhost:8000/aws/buckets",{
-            method:"POST",
-            body:JSON.stringify(this.state.credential),
+    const navigateHome = () => {
+        // 👇️ navigate to /
+        navigate('/');
+      };
+  
+    const getData = () => {
+        fetch("http://localhost:8000/aws/buckets", {
+            method: "POST",
+            body: JSON.stringify(credential),
             headers: {
                 "Content-Type": "application/json",
             },
         })
-        .then((res)=>res.json())
-        .then((res)=>{
-            if(res.error){
-                throw new Error(res.error);
-            }
-            this.setState({allBuckets:res.buckets})
-            this.setState({errors:{status:false,msg:this.state.errors.msg}})
-            console.log(res)
-            
-        })
-        .catch((err)=>{
-            console.log(err)
-            this.setState({errors:{status:true,msg:err.toString()}})
-        })
-    }
-    componentDidMount(){
-        
-    }
-    renderMap=()=>{
-        let buckets=[]
-        for (const [key, value] of Object.entries(this.state.allBuckets)){
-            let isPublic="N/A";
-            if(value.Policy){
-                isPublic=value.Policy.IsPublic?"True":"False"
-            }
-            buckets.push (
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.error) {
+                    throw new Error(res.error);
+                }
+                console.log("before set buckets")
+                console.log(res.buckets);
+                setAllBuckets(res.buckets);
+                setErrors({ status: false, msg: errors.msg });
+            })
+            .catch((err) => {
+                console.log(err);
+                setErrors({ status: true, msg: err.toString() });
+            });
+    };
+
+    useEffect(() => {
+        setLoading(true);
+        // Call your API here to fetch data if needed
+        setLoading(false);
+    }, []);
+
+    let renderMap = () => {
+        const buckets = [];
+    
+        for (const bucketName in allBuckets) {
+            const value = allBuckets[bucketName];
+            const isPublic = value.Policy ? (value.Policy.IsPublic ? "True" : "False") : "N/A";
+    
+            buckets.push(
                 <li
                     key={value.Name}
                     style={
                         isPublic === "N/A"
-                        ? { backgroundColor: "blue" }
-                        : isPublic === "True"
-                        ? { backgroundColor: "green" }
-                        : { backgroundColor: "red" }
+                            ? { backgroundColor: "blue" }
+                            : isPublic === "True"
+                            ? { backgroundColor: "green" }
+                            : { backgroundColor: "red" }
                     }
-                    >
+                >
                     <NavLink className="dropdown-item" to={`/buckets/objects/${value.Name}`}>
-                {value.Name}
-                </NavLink>
+                        {value.Name}
+                    </NavLink>
                 </li>
-        )
+            );
         }
-        return buckets
-    }
-    render(){
+    
+        return buckets;
+    };
+    
 
-        return (
-            
-            <div className="row">
-                {/* <h1 className="col-12 justify-content-center">buckets</h1> */}
-                <div className="col">
+    return (
+        <div className="row">
+            <div className="col">
                 <div className="dropdown">
                     <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    buckets
+                        buckets
                     </button>
                     <ul className="dropdown-menu">
-                    {this.state.allBuckets.length < 1 && this.state.loading ? (
-                        <div className="mx-2">Loading buckets...</div>
-                    ) : (
-                        this.renderMap()
-                    )}
+                        {allBuckets.length < 1 && loading ? <div className="mx-2">Loading buckets...</div> : renderMap()}
                     </ul>
                 </div>
                 <div className="row mt-2">
-                <span className="col-12"><span style={{ backgroundColor: "blue",width:"10px", height:"10px" ,display:"inline-block"}}></span> access policy:none </span>
-                <span className="col-12"><span style={{ backgroundColor: "green" ,width:"10px", height:"10px" ,display:"inline-block" }}></span> access policy: isPublic =true </span>
-                <span className="col-12"><span style={{ backgroundColor: "red" ,width:"10px", height:"10px" ,display:"inline-block" }}></span> access policy: isPublic =false </span>
-                
+                    <span className="col-12">
+                        <span style={{ backgroundColor: "blue", width: "10px", height: "10px", display: "inline-block" }}></span> access policy: none
+                    </span>
+                    <span className="col-12">
+                        <span style={{ backgroundColor: "green", width: "10px", height: "10px", display: "inline-block" }}></span> access policy: isPublic = true
+                    </span>
+                    <span className="col-12">
+                        <span style={{ backgroundColor: "red", width: "10px", height: "10px", display: "inline-block" }}></span> access policy: isPublic = false
+                    </span>
                 </div>
-                </div>
-                <div className="col my-3">
+            </div>
+            <div className="col my-3">
                 <h3>aws credentials</h3>
                 <div className="form">
                     <div className="mb-3">
-                        <label htmlFor="accessId"  className="form-label">Access Id</label>
-                        <input type="password" onChange={this.getAccessId} className="form-control" id="accessId" />
+                        <label htmlFor="accessId" className="form-label">
+                            Access Id
+                        </label>
+                        <input type="password" onChange={getAccessId} className="form-control" id="accessId" />
                     </div>
-                    
+
                     <div className="mb-3">
-                    
-                        <label htmlFor="secretAccessId" className="form-label" >Secret Access Id</label>
-                        <input type="password" onChange={this.getSecretAccessId} className={`form-control`} id="secretAccessId"/>
-                        {this.state.errors.status?(
-                            <div className="text-danger fs-6">
-                                {this.state.errors.msg}
-                            </div>
-                            ):(!!this.state.errors.msg?(
-                                <div className="text-success fs-6">
-                                    every thing looks good.
-                                </div>
-                            )
-                            :("")
-                        )}
+                        <label htmlFor="secretAccessId" className="form-label">
+                            Secret Access Id
+                        </label>
+                        <input type="password" onChange={getSecretAccessId} className={`form-control`} id="secretAccessId" />
+                        {errors.status ? <div className="text-danger fs-6">{errors.msg}</div> : !!errors.msg ? <div className="text-success fs-6">Every thing looks good.</div> : ""}
                     </div>
-                    {"msg: "+!!this.state.errors.msg}
-                    <br/>
-                    {"status: "+this.state.errors.status}
-                    <br/>
-                    <button type="submit" className="btn btn-dark px-4" onClick={this.submit}>Submit</button>
+                    <button type="submit" className="btn btn-dark px-4" onClick={submit}>
+                        Submit
+                    </button>
                 </div>
-                </div>
-                <Outlet context={this.state.credential} />
-                
             </div>
-        )
-    }
+            <Outlet context={credential} />
+        </div>
+    );
 }
 
 export default Buckets;
--- react/aws/src/Components/objects/objects.js
+++ react/aws/src/Components/objects/objects.js
@@ -1,83 +1,89 @@
 
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, NavLink } from "react-router-dom";
 import { useOutletContext } from "react-router-dom";
 
-let Objects=()=>{
-    let credential=useOutletContext()
-    console.log(credential)
-    let {bucket} = useParams();
-    let [objects,setObjects]=useState([])
-    useEffect(()=>{
-        
-        fetch(`http://localhost:8000/aws/objects/${bucket}`,{
-            method:"POST",
-            body:JSON.stringify(credential),
+function Objects() {
+    const [objects, setObjects] = useState([]);
+    const [errors, setErrors] = useState({ status: false, msg: "" });
+    const { bucket } = useParams();
+    const credential = useOutletContext();
+
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/aws/objects/${bucket}`, {
+            method: "POST",
+            body: JSON.stringify(credential),
             headers: {
                 "Content-Type": "application/json",
             },
         })
-        .then((res)=>res.json())
-        .then((res)=>{
-            console.log(res.objects)
-            setObjects(res.objects)
-        })
-        .catch((err)=>{console.log(err)})
-    }, [bucket])
-    let renderMap=()=>{
-            return objects.map((obj,index)=>{
-                let span=obj.Acl.Grants.length
-                return (
-
-                    <tr key={obj.Key}>
-                            <th scope="row" >{index+1}</th>
-                            <td >{obj.Key}</td>
-                            <th >users</th>
-                            
-                    { obj.Acl.Grants.map((access,index)=>{
-                            return(
-                                <td key={index}>
-                                    <table className="table">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col" >user name</th>
-                                            <th scope="col" >Permission</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>{access.Grantee.DisplayName}</td>
-                                                <td>{access.Permission}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            )
-                        
-                    })}
-                    </tr>
-                )
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.error) {
+                    setErrors(res.error);
+                    throw new Error(res.error);
+                }
+                setObjects(res.objects);
             })
-            
-        }
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [bucket,credential]);
+
+    const renderGrants = (grants) => {
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">User name</th>
+                        <th scope="col">Permission</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {grants.map((access, index) => (
+                        <tr key={index}>
+                            <td>{access.Grantee.DisplayName}</td>
+                            <td>{access.Permission}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    };
+
+    const renderMap = () => {
+        return objects.map((obj, index) => (
+            <tr key={obj.Key}>
+                <th scope="row">{index + 1}</th>
+                <td>{obj.Key}</td>
+                <td colSpan={3}>
+                    {obj.Acl.Grants.length > 0 ? (
+                        renderGrants(obj.Acl.Grants)
+                    ) : (
+                        <span>No access permissions</span>
+                    )}
+                </td>
+            </tr>
+        ));
+    };
 
     return (
         <div className="d-flex">
-            <table className="table" >
+            <table className="table">
                 <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col" colSpan={3}>Acl</th>
-                </tr>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col" colSpan={3}>
+                            Acl
+                        </th>
+                    </tr>
                 </thead>
-                <tbody>
-
-                    {renderMap()}
-                </tbody>
+                <tbody>{renderMap()}</tbody>
             </table>
         </div>
-    )
+    );
 }
 
 export default Objects;
